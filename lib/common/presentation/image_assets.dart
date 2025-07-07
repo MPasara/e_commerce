8262706ecb669e18d8b3a3268da5e88ff83153f8
Mladen <mladen.pasara@q.agency>
2025-07-07 12:11:30 +0200

    ID: 0 - Added sad pepe gif to empty product list

--- lib/common/presentation/image_assets.dart
+++ lib/common/presentation/image_assets.dart
@@ -1,6 +1,7 @@
 const String imagePath = 'assets/images';
+const String gifsPath = 'assets/gifs';
 
 abstract class ImageAssets {
   static const String imageFileName = '$imagePath/<image_file_name>';
-  static const String sadPepe = '$imagePath/sad_pepe.png';
+  static const String sadPepe = '$gifsPath/sad_pepe.gif';
 }
